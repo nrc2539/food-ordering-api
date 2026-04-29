ab2ef2fb8f060c979565d06092f412c14358cfb6
nrc-dev <narongchai.b2539@gmail.com>
2026-04-29 14:14:11 +0700

    fix: set global prefix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('/api');
   await app.listen(process.env.API_PORT || 8080);
 }
 bootstrap();
