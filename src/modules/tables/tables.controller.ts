53c451c19d08816d77afd17492760446fb38f41a
nrc-dev <narongchai.b2539@gmail.com>
2026-05-04 12:55:58 +0700

    fix: role guard in table controller

--- src/modules/tables/tables.controller.ts
+++ src/modules/tables/tables.controller.ts
@@ -15,12 +15,13 @@ import { RolesGuard } from '../access-control/guards/roles.guard';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Roles } from '../access-control/decorators/roles.decorator';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles('admin')
+@UseGuards(JwtAuthGuard)
 @Controller('tables')
 export class TablesController {
   constructor(private readonly tablesService: TablesService) {}
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Post()
   create(@Body() createTableDto: CreateTableDto) {
     return this.tablesService.create(createTableDto);
@@ -31,16 +32,22 @@ export class TablesController {
     return this.tablesService.findAll();
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.tablesService.findOne(+id);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTableDto: UpdateTableDto) {
     return this.tablesService.update(+id, updateTableDto);
   }
 
+  @UseGuards(RolesGuard)
+  @Roles('admin')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tablesService.remove(+id);
